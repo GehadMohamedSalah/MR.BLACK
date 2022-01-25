a54d84f297b276ab28de491afe3422be89eea796
Gehad Darwish <gehadderwish@gmail.com>
2022-01-25 14:03:38 +0200

    updates

--- V2/MRBLACK/Areas/Identity/Data/IdentitySetupContext.cs
+++ V2/MRBLACK/Areas/Identity/Data/IdentitySetupContext.cs
@@ -13,10 +13,21 @@ namespace MRBLACK.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<IdentitySetupUser>(entity =>
+            {
+                entity.Property(c => c.Code).ValueGeneratedOnAdd();
+            });
+
+            builder.Entity<IdentitySetupRole>(entity =>
+            {
+                entity.Property(c => c.Code).ValueGeneratedOnAdd();
+            });
+
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
         }
         DbSet<IdentitySetupRole> IdentitySetupRole { get; set; }
     }
